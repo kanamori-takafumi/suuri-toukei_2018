25a0aecdcb1e6727031db5e0f815effe0e83b5e8
T. KANAMORI <35907655+kanamori-takafumi@users.noreply.github.com>
2018-10-19 10:02:34 +0900

    Add files via upload

--- bloodtype.r
+++ bloodtype.r
@@ -1,18 +1,19 @@
-## theta <- c(thetaA,thetaB); n <- c(nA,nB,nAB,nO). 
-nlikelihood <- function(theta,n){## 対数尤度を計算する関数
+# theta <- c(thetaA,thetaB); n <- c(nA,nB,nAB,nO). 
+nlikelihood <- function(theta,n){# 負の対数尤度を計算
   a <- theta[1]; b <- theta[2]; o <- 1-a-b
   l <- log(c(a^2+2*a*o, b^2+2*b*o, 2*a*b, o^2))
   -sum(n*l)
 }
 
-## 最尤推定量(mle)を求める関数
-## n <- c(nA,nB,nAB,nO). 
+# 最尤推定量(mle)を求める
+# n <- c(nA,nB,nAB,nO). 
 mle <- function(n){
-  ## 最適化の計算
+  # 最適化の計算
   op <- optim(c(1/3,1/3),nlikelihood,n=n) 
-  ## 解を出力
+  # 解を出力
   list(a=op$par[1],b=op$par[2],o=1-op$par[1]-op$par[2])
 } 
+
 
 n <- c(40,30,10,20)
 r <- mle(n); ta <- r$a; tb <- r$b; to <- r$o
